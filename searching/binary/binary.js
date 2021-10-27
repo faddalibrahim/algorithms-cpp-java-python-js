4e61f1dd905523c45e92708d19d54d880e933f1d
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-10-27 19:50:47 +0000

    js binary search - recursive & iterative

--- searching/binary/binary.js
+++ searching/binary/binary.js
@@ -1,24 +1,46 @@
-const nums = [3, 5, 6, 9, 11, 18, 20, 21, 24, 30];
+const array = [3, 5, 6, 9, 11, 18, 20, 21, 24, 30];
 
-function binarySearch(
-  numsArray,
-  numFind,
+function binarySearchRecursive(
+  array,
+  target,
   start = 0,
-  end = numsArray.length - 1
+  end = array.length - 1
 ) {
   let middle = Math.floor((start + end) / 2);
 
-  if (numsArray[middle] === numFind) {
+  if (array[middle] === target) {
     return middle;
-  } else if (numsArray[middle] > numFind) {
+  } else if (array[middle] > target) {
     end = middle - 1;
-    return binarySearch(numsArray, numFind, start, end);
+    return binarySearchRecursive(array, target, start, end);
   } else {
     start = middle + 1;
-    return binarySearch(numsArray, numFind, start, end);
+    return binarySearchRecursive(array, target, start, end);
   }
 }
 
-console.log(binarySearch(nums, 30)); // 2
-console.log(binarySearch(nums, 18)); // 5
-console.log(binarySearch(nums, 21)); // 7
+console.log(binarySearchRecursive(array, 3)); // 0
+console.log(binarySearchRecursive(array, 6)); // 2
+console.log(binarySearchRecursive(array, 20)); // 6
+
+function binarySearchIterative(
+  array,
+  target,
+  start = 0,
+  end = array.length - 1
+) {
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (array[middle] === target) {
+      return middle;
+    } else if (array[middle] > target) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+}
+
+console.log(binarySearchIterative(array, 30)); // 9
+console.log(binarySearchIterative(array, 18)); // 5
+console.log(binarySearchIterative(array, 21)); // 7
