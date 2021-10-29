2e84181f3c257d21a21f534505a69ca212c42842
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-10-29 00:37:25 +0000

    binary java in progress

---
+++ searching/binary/binary.java
@@ -0,0 +1,46 @@
+package searching.binary;
+import java.lang.Math;
+
+public class Binary {
+    
+    public static void main(String[] args){
+
+    }
+
+    public int recursive(int[] array, int target, int start, int end) {
+        start = 0;
+        end = array.length - 1;
+
+        int middle = (int) Math.floor((start + end) / 2);
+
+        if (start > end) return -1;
+
+        if (array[middle] == target) {
+            return middle;
+        } else if (array[middle] > target) {
+            end = middle - 1;
+            return recursive(array, target, start, end);
+        } else if (array[middle] < target) {
+            start = middle + 1;
+            return recursive(array, target, start, end);
+        }
+    }
+
+    public int iterative(int[] array, int target, int start, int end) {
+        start = 0;
+        end = array.length - 1;
+        while (start <= end) {
+            int middle = (int) Math.floor((start + end) / 2);
+
+            if (array[middle] == target) {
+                return middle;
+            } else if (array[middle] > target) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+}
