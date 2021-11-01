3ec4912af7e0cce3812358b6ccc42e9e6189e809
Faddal Ibrahim <faddalibrahim@gmail.com>
2021-11-01 15:14:53 +0000

    memoized canSum

--- dynamic-programming/canSum.js
+++ dynamic-programming/canSum.js
@@ -21,32 +21,37 @@
 
 // O(n) using hash map
 // O(n) space complexity
-const canSum = (targetSum, numbers) => {
-  const remainders = {};
-  for (number of numbers) {
-    remainders[number] = targetSum - number;
-    if (remainders[number] in remainders) return true;
-  }
-  return false;
-};
-
-console.log(canSum(12, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(3, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(10, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(15, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(13, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(7, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(100, [1, 2, 3, 4, 6, 7, 8]));
-console.log(canSum(8, [1, 2, 3, 4, 6, 7, 8]));
+// const canSum = (targetSum, numbers) => {
+//   const remainders = {};
+//   for (number of numbers) {
+//     remainders[number] = targetSum - number;
+//     if (remainders[number] in remainders) return true;
+//   }
+//   return false;
+// };
 
 // using recursion
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+
   if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
 
   for (let num of numbers) {
     const remainder = targetSum - num;
-    if (canSum(remainder, numbers)) return true;
+
+    if (canSum(remainder, numbers, memo) === true) {
+      memo[targetSum] = true;
+      return true;
+    }
   }
 
+  memo[targetSum] = false;
   return false;
 };
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [5, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
