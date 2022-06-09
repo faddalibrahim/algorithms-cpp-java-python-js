fbdbf1d8cf28daacc81d32595cb7768ae97662de
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-06-09 05:41:16 +0000

    add insertion sort

--- TODO.md
+++ TODO.md
@@ -4,3 +4,4 @@
 2. comments
 3. illustrations/animations
 4. Resources to learn more
+5. add implementation for other languages
---
+++ sorting/insertion_sort/insertion-sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        curr = arr[i]
+        j = i - 1
+
+        while curr < arr[j] and j > -1:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = curr
+
+    return arr
+
+print(insertion_sort([8,9,7,5,0,3,18,12]))
